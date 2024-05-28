289a927b5a9e85783c2f28af62eb7375b8fbacdb
brunowhoami <151976135+brunowhoami@users.noreply.github.com>
2024-05-28 19:56:53 -0300

    Adicionando if & else

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 alert("Boas vindas ao jogo do numero secreto");
+
+let numeroSecreto = 5;
+let chute = prompt("Escolha um número entre 1 e 10");
+
+    if(chute == numeroSecreto) {
+        alert("Parabens você acertou (5)");
+    }else{
+        alert("Infelizmente você errou :( ");
+    }
