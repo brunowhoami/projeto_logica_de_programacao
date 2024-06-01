09119e2bda3f19dcbda8dbbecf7cf2ec2363ff0c
brunowhoami <brnsmgspdmkr@gmail.com>
2024-06-01 20:30:51 -0300

    adicionando while

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 alert("Boas vindas ao jogo do numero secreto");
 
 let numeroSecreto = 5;
-let chute = prompt("Escolha um número entre 1 e 10");
+let chute;
 
+while(chute != numeroSecreto){
+    chute = prompt("Escolha um número entre 1 e 10");
     if(chute == numeroSecreto) {
         alert(`Parabens você acertou o numero secreto ${numeroSecreto}`);
-    }else{
-        alert("Infelizmente você errou :( ");
+    } else {
+        if(chute > numeroSecreto){
+        alert(`O número secreto é menor que ${chute}`);
+    } else {
+        alert(`O numero secreto é maior que ${chute}`);
     }
+}
+}
