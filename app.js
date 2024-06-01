47d7c891b976997e5cab409068fa18948e938000
Bruno Oliveira <brnsmgspdmkr@gmail.com>
2024-06-01 19:03:51 -0300

    alterando o alert

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ let numeroSecreto = 5;
 let chute = prompt("Escolha um número entre 1 e 10");
 
     if(chute == numeroSecreto) {
-        alert("Parabens você acertou (5)");
+        alert(`Parabens você acertou o numero secreto ${numeroSecreto}`);
     }else{
         alert("Infelizmente você errou :( ");
     }
