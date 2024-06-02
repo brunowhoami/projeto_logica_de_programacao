e8103f6a075f3417fed3aae2b7f0bbc534050b51
brunowhoami <brnsmgspdmkr@gmail.com>
2024-06-02 16:14:02 -0300

    novo modo de fazer o if

--- app.js
+++ app.js
@@ -2,16 +2,29 @@ alert("Boas vindas ao jogo do numero secreto");
 
 let numeroSecreto = 5;
 let chute;
+let tentativas = 1;
 
-while(chute != numeroSecreto){
-    chute = prompt("Escolha um número entre 1 e 10");
-    if(chute == numeroSecreto) {
-        alert(`Parabens você acertou o numero secreto ${numeroSecreto}`);
-    } else {
-        if(chute > numeroSecreto){
-        alert(`O número secreto é menor que ${chute}`);
-    } else {
-        alert(`O numero secreto é maior que ${chute}`);
+    // Enquanto
+    while(chute != numeroSecreto){
+        chute = prompt("Escolha um número entre 1 e 10");
+        if(chute == numeroSecreto) {
+            break;   
+        } else {
+            if(chute > numeroSecreto){
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O numero secreto é maior que ${chute}`);
+        }
+        // tentativas = tentativas + 1;
+        tentativas++;
+        }
     }
-}
-}
+
+let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa"
+    alert(`Parabens você acertou o numero secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
+
+        // if(tentativas > 1){
+        //     alert(`Parabens você acertou o numero secreto ${numeroSecreto} com ${tentativas} tentativas`);
+        // }else{
+        //     alert(`Parabens você acertou o numero secreto ${numeroSecreto} com ${tentativas} tentativa`);
+        // }
